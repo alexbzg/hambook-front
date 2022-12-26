bdd041ec96bf1c66b58e67b7e060bccacd1420d7
root <alexbzg@gmail.com>
2022-12-26 14:32:50 +0300

    re-inititlaize global redux state on userLogout

--- src/App/store.js
+++ src/App/store.js
@@ -1,16 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit'
 
 import { default as authReducer, fetchUserFromToken } from '../features/auth/authSlice'
 import profileReducer from '../features/profile/profileSlice'
 import logsReducer from '../features/logbook/logsSlice'
 
-
-const store = configureStore({
-  reducer: {
+const combinedReducer = combineReducers({
     auth: authReducer,
     profile: profileReducer,
     logs: logsReducer
+})
+
+const rootReducer = (state, action) => {
+  if (action.type === 'auth/userLogout') {
+    state = undefined;
   }
+  return combinedReducer(state, action);
+}
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [...getDefaultMiddleware()]
 })
 
 store.dispatch(fetchUserFromToken())
